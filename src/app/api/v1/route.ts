9385e599c251ff4d1d37efd8b3f4d33aeacdca78
Eric Rojas Fuerte <erojasfuerte@gmail.com>
2026-06-14 08:12:34 +0200

    Sprint 26: test: custom header passthrough

--- src/app/api/v1/route.ts
+++ src/app/api/v1/route.ts
@@ -1,3 +1,7 @@
 import { apiOk, withTiming } from '@/lib/api/response'
 
-export const GET = withTiming(() => apiOk({ version: 'v1', status: 'ok' }))
+export const GET = withTiming(() => {
+  const res = apiOk({ version: 'v1', status: 'ok' })
+  res.headers.set('X-Test', 'works')
+  return res
+})
